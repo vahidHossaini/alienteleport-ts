dd7e2234e19c9fda48ca093f79e4ddf73a3fe559
Destiny World <74211870+DestinyDevTeam@users.noreply.github.com>
2023-12-04 11:35:19 +0100

    Reset

--- storageService.ts
+++ storageService.ts
@@ -6,22 +6,21 @@ export default class StorageService
     static defaultTime:string=process.env.DFLT_TIME;
     static async Get():Promise<StorageModel>
     {
-       var time=  new StorageModel({}) ;
+       var time = new StorageModel({}) ;
        if(fs.existsSync('time.txt'))
        {
-            time=new StorageModel(JSON.parse(fs.readFileSync('time.txt')+'')) 
+            time = new StorageModel(JSON.parse(fs.readFileSync('time.txt')+'')) 
        }
-       // Returing time from Process Environment using format 2018-07-08T00:00:00.000Z
-       return new StorageModel(JSON.parse(process.env.DFLT_TIME));
+       return time;
     }
     static async Save(name:string,value:any)
     { 
-        var time=  new StorageModel({}) ;
+        var time = new StorageModel({}) ;
         if(fs.existsSync('time.txt'))
         {
-             time=new StorageModel(JSON.parse(fs.readFileSync('time.txt')+'')) 
+             time = new StorageModel(JSON.parse(fs.readFileSync('time.txt')+'')) 
         }
-        time[name]=value;
+        time[name] = value;
         fs.writeFileSync('time.txt',JSON.stringify(time)) 
     }
 }
