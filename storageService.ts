353e9a8a2f1da4498104c3f5fb6b15e12eb669ec
Destiny World <74211870+DestinyDevTeam@users.noreply.github.com>
2023-12-04 11:15:17 +0100

    Change Start Time to process environment
    Instead of time.txt

--- storageService.ts
+++ storageService.ts
@@ -2,7 +2,8 @@ import StorageModel from "./models/storageModel";
 import fs from 'fs';
 export default class StorageService
 {
-    static defaultTime:string='2018-07-08T00:00:00.000Z';
+    {/* Changed to Process Environment for Static Time using format 2018-07-08T00:00:00.000Z */}
+    static defaultTime:string=process.env.DFLT_TIME;
     static async Get():Promise<StorageModel>
     {
        var time=  new StorageModel({}) ;
@@ -10,7 +11,8 @@ export default class StorageService
        {
             time=new StorageModel(JSON.parse(fs.readFileSync('time.txt')+'')) 
        }
-       return time;
+        {/* Returing time from Process Environment using format 2018-07-08T00:00:00.000Z */}
+       return process.env.DFLT_TIME;
     }
     static async Save(name:string,value:any)
     { 
